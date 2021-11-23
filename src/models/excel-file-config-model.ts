d9fba5f3fd684f52c7254319dbd2071d602d30dc
Joseph Chingalo <profschingalo@gmail.com>
2021-11-23 13:47:01 +0300

    feat: supportive model for excel file generations

---
+++ src/models/excel-file-config-model.ts
@@ -0,0 +1,9 @@
+export interface ExcelFileConfigModel {
+  inputMappingFileName: string;
+  inputMappingFileSheetName: string;
+  inputMappingOldReferenceColumnName: string;
+  inputMappingNewReferenceColumnName: string;
+  inputDataFileName: string;
+  inputDataReferenceColumnName: string;
+  outputDataFileName: string;
+}

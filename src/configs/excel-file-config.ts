ff906e7ede9cb0be5db0097296ae9a9599796f74
Joseph Chingalo <profschingalo@gmail.com>
2021-11-23 14:02:57 +0300

    feat: adding configs for excel file configs

---
+++ src/configs/excel-file-config.ts
@@ -0,0 +1,14 @@
+import { ExcelFileConfigModel } from '../models/excel-file-config-model';
+
+export const EXCEL_FILE_CONFIG: ExcelFileConfigModel = {
+  inputMappingFileName: 'Somalia_Tools_Metadata_Mapping',
+  inputMappingFileExtension: 'xlsx',
+  inputMappingFileSheetName: 'sql MF-04',
+  inputMappingOldReferenceColumnName: 'Old data element uid',
+  inputMappingNewReferenceColumnName: 'New Data Element UID',
+  inputDataFileName: 'MF-04_data',
+  inputDataFileExtension: 'csv',
+  inputDataDataElementReferenceColumnName: 'dataelement',
+  inputDataCategoryComboReferenceColumnName: 'categoryoptioncombouid',
+  outputDataFileName: '[Mapped] MF-04_data'
+};
